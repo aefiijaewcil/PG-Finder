4e7f0b808bdd2d06ebc7727a357060285536b8c0
pcdac05 <pcdac055@gmail.com>
2025-02-02 17:36:42 +0530

    5th commit added reviews

---
+++ src/main/java/com/pgfinder/repo/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.pgfinder.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pgfinder.entity.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    
+}
